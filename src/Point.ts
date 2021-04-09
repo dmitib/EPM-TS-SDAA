2e9ff7bc18b46333372df66c9c1d5a3d37b1038d
Dmitrii Gushchin <Dmitrii_Gushchin@epam.com>
2021-04-09 17:06:15 +0300

    module-1-hw-in-process initial commit

--- src/Point.ts
+++ src/Point.ts
@@ -1,1 +1,23 @@
-export class Point {}
+export class Point {
+    public x: number;
+    public y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x || 0;
+        this.y = y || 0;
+    }
+
+    public toString(): string {
+        return `(${this.x}, ${this.y})`
+    }
+
+    public distance(xOrPoint?: number | Point): number;
+    public distance(xOrPoint?: number | Point, y?: number): number;
+    public distance(xOrPoint?: number | Point, y?: number): number {
+        if (xOrPoint instanceof Point) {
+            return xOrPoint.distance(this.x, this.y);
+        }
+
+        return  Math.round(Math.sqrt((this.y ** 2) + (this.x ** 2)));
+    }
+}
--- src/Shape.ts
+++ src/Shape.ts
@@ -1,3 +1,45 @@
+import { Point } from "./Point";
+
 export abstract class Shape {
+    protected color: string; 
+    protected filled: boolean;
+    protected points: Point[];
+
     abstract getType(): string;
+
+    constructor();
+    constructor(points: Point[], color: string, filled: boolean);
+    constructor(points?: Point[], color?: string, filled?: boolean) {
+        if (points.length < 3) {
+            throw new Error("points array less than 3");
+        }
+
+        this.color = color || "green";
+        this.filled = filled ?? true;
+        this.points = points;
+    };
+
+    public toString(): string {
+        let processedPoints = this.addSpace(this.points);
+
+        return `A Shape with color of ${this.color} and ${!this.filled ? "not " : ""}filled. Points:${processedPoints}.`
+    }
+
+    public getPerimeter(): number {
+        let lastPoint: Point = this.points[this.points.length - 1];
+
+        return lastPoint.x * lastPoint.y;
+    }
+
+    public addSpace(points: Point[]): string[] {
+        let result: string;
+        let acc: string[] = [];
+
+        points.forEach(point => {
+            result = ` (${point.x}, ${point.y})`;
+            acc.push(result);
+        });
+
+        return acc;
+    }
 }
